cd4a8d61f3d0d0fea1109f0c4c9423c116b21230
Nenad Brcic <neno@neno.ch>
2020-08-08 14:08:00 +0200

    Use variable for timeout

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 import "../styles/index.scss"
 import Collapsible from "./libs/Collapsible"
 
+const timeout = process.env.STORYBOOK_TIMEOUT || 0
+console.log("My timeout", timeout)
+
 setTimeout(() => {
   const el = document.querySelector("[data-module='collapsible']")
   if (el) {
     new Collapsible(el)
   }
-}, 2000)
+}, timeout)
