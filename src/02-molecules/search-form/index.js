cf9bb6607a0bfe8d133f0b527e27010d45db83c1
Josip Anic <josipanic1985@gmail.com>
2020-08-10 15:35:00 +0200

    Creating search form molecule

---
+++ src/02-molecules/search-form/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchForm = ({ pages = [], currentPage }) => {
+  return (
+    <form className='m-search-form'>
+
+    </form>
+  );
+};
+
+export default SearchForm;
