ceb9a29503aba803344363d8c88d83c99fb65a52
Josip Anic <josipanic1985@gmail.com>
2020-08-20 12:19:01 +0200

    Add two headers example

--- src/01-atoms/table/stories/table.data.js
+++ src/01-atoms/table/stories/table.data.js
@@ -22,8 +22,30 @@ export const sideHeaderTableData = {
     ['Extra Heading']
   ],
   data: [
-    [ '2017', '335 BehiG-konform', '58 Prozent', '-', 'Lorem'],
-    [ '2023', '556 BehiG-konform', '93 Prozent', '-', 'Lorem'],
-    [ 'Nach 2023', '679 BehiG-konform', '99 Prozent', '-', 'Lorem']
+    ['2017', '2023', 'Nach 202'],
+    ['335 BehiG-konform', '556 BehiG-konform', '93 Prozent'],
+    ['358 Prozent', '679 BehiG-konform', '99 Prozent'],
+    ['-', '-', '-'],
+    ['Lorem', 'Ipsum', 'Dolor sit amet']
+  ]
+}
+
+export const twoHeadersTableData = {
+  columns: [
+    ['Jahr'],
+    ['Anzahl Bahnhöfe*'], 
+    ['Anteil Reisender'],
+    ['Extra Heading'],
+    ['Extra Heading']
+  ],
+  rows: [
+    ['2017'],
+    ['2023'], 
+    ['Nach 2023'],
+  ],
+  data: [
+    ['335 BehiG-konform', '58 Prozent', '-', 'Lorem'],
+    ['556 BehiG-konform', '93 Prozent', '-', 'Lorem'],
+    ['679 BehiG-konform', '99 Prozent', '-', 'Lorem']
   ]
 }
